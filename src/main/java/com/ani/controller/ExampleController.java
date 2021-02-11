e53ad3c9d00c43f459f0326cf6f7fce16ad60eb8
canv <aleksandrinstall@gmail.com>
2021-02-11 19:41:07 +0300

    swap Thymleaf to Mustache

--- src/main/java/com/ani/controller/ExampleController.java
+++ src/main/java/com/ani/controller/ExampleController.java
@@ -6,9 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ExampleController {
@@ -16,19 +18,37 @@ public class ExampleController {
     public List<VideoExample> exampleList = new ArrayList<>();
 
     @GetMapping("/greeting")
-    public String greetingForm(Model model) {
+    public String greetingForm(Map<String, Object> model) {
         VideoExample videoExample;
 
         if(exampleList.size()!=0) videoExample = exampleList.get(exampleList.size()-1);
-        else videoExample = new VideoExample();
-        model.addAttribute("videoExample", videoExample);
+        else {
+            videoExample = new VideoExample();
+            videoExample.setId(0);
+            videoExample.setContent("empty videoExample");
+        }
+
+//        model.put("id", videoExample.getId());
+//        model.put("content", videoExample.getContent());;
+        model.put("videoExample", videoExample);;
         return "greeting";
     }
 
     @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute VideoExample videoExample, Model model) {
+    public String greetingSubmit(
+            @RequestParam(name="id", required = false, defaultValue = "0") String id,
+            @RequestParam(name="content", required = false, defaultValue = "FixMe") String content,
+            Map<String, Object> model
+    ) {
+        VideoExample videoExample = new VideoExample();
+        videoExample.setId(Long.parseLong(id));
+        videoExample.setContent(content);
+
         exampleList.add(videoExample);
-        model.addAttribute("videoExample", videoExample);
+
+//        model.put("id", id);
+//        model.put("content", content);
+        model.put("videoExample", videoExample);;
         return "greeting";
     }
 
